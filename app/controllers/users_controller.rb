3fcf0474a341aa65baae9028ce762653254c8721
Taichi Watabe <taichi722neville@gmail.com>
2020-04-02 18:32:05 +0000

    update files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      # 保存の成功をここで扱う。
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-=======
   
     def show
       @user = User.find(params[:id])
@@ -50,5 +24,4 @@ class UsersController < ApplicationController
         params.require(:user).permit(:name, :email, :password,
                                      :password_confirmation)
       end
->>>>>>> sign-up2
 end
